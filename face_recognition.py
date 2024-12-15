7f2735f4278562717cf296317ea4945fc9eaa4ad
Aissiou Ikram <88051280+AiIkram@users.noreply.github.com>
2024-12-15 20:23:26 +0100

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -21,7 +21,7 @@ def main():
         return
 
     # Video Capture Setup
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
     if not cap.isOpened():
         st.error("Unable to access the webcam. Ensure your camera is connected and permissions are granted.")
         return
