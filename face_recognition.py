c6b7c7e682e9d117938eec8b18d754dddaa85785
Aissiou Ikram <88051280+AiIkram@users.noreply.github.com>
2024-12-16 08:50:47 +0100

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,12 +1,9 @@
 import cv2
 import streamlit as st
 import tempfile
-import numpy as np
 
 def main():
     # Page Configuration for Streamlit
-    st.set_page_config(page_title="Facial Recognition", layout="centered")
-    st.title("Facial Recognition Web App")
     st.set_page_config(page_title="Facial Detection", layout="centered")
     st.title("Facial Detection Web App")
     st.caption("Powered by OpenCV and Streamlit")
@@ -47,7 +44,6 @@ def main():
                 st.warning("Failed to grab a frame or video finished. Exiting...")
                 break
 
-            # Convert the frame to grayscale
             # Convert the frame to grayscale for face detection
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -56,15 +52,14 @@ def main():
                 gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
             )
 
-            # Draw rectangles around detected faces
             # Draw rectangles around detected faces (Face Detection)
             for (fx, fy, fw, fh) in face_coordinates:
                 cv2.rectangle(frame, (fx, fy), (fx + fw, fy + fh), (0, 255, 0), 2)
 
             # Convert the BGR frame to RGB (Streamlit expects RGB format)
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
             # Display the frame in Streamlit
-            frame_placeholder.image(frame, channels="BGR", use_column_width=True)
             frame_placeholder.image(frame_rgb, channels="RGB", use_container_width=True)
 
             # Check if the stop button is pressed
